b773f17894c905b6254013491a60d241ac2e5f66
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-04-18 16:27:28 +0200

    correct kevin

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn
 import sklearn
 
-datax = pd.read_csv('csv_files/Data_X.csv')
-datay = pd.read_csv('csv_files/Data_Y.csv')
-df = pd.merge(datax, datay, on='ID', how='inner')
+X = pd.read_csv('csv_files/Data_X.csv')
+Y = pd.read_csv('csv_files/Data_Y.csv')
+df = pd.merge(X, Y, on='ID', how='inner')
 
 print(df)
