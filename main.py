b01c8020a6314666c97f19ecece4ec08651d3573
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-04-18 16:42:26 +0200

    detect null values

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn
 import sklearn
+import numpy as np
 
 X = pd.read_csv('csv_files/Data_X.csv')
 Y = pd.read_csv('csv_files/Data_Y.csv')
 df = pd.merge(X, Y, on='ID', how='inner')
 
 print(df)
+
+null_indexes = np.where(pd.isnull(df))
