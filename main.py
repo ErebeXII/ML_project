402b22a2fce87505e0acee971b886b7f5d1f65b0
yoyo53 <94387950+yoyo53@users.noreply.github.com>
2023-05-06 17:01:33 +0200

    models

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import random
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import spearmanr
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_predict, KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.neighbors import KNeighborsRegressor
@@ -42,24 +44,28 @@ for i in range(len(corr_mat)):
 # get relevant columns to analyze
 input_cols = [e for e in df.columns if e not in ('ID', 'DAY_ID', 'TARGET')]
 
+# split data for cross validation
+cv = KFold(n_splits=10, shuffle=True, random_state=random.randint(1, 100))
+
+
 # get the best number of components for PCA
-"""
-lin_reg = LinearRegression()
-y = []
-for i in range(1, len(input_cols)):
-    pca = PCA(n_components=i)
-    X_train_pc = pca.fit_transform(df[input_cols])
-    y_predict = cross_val_predict(lin_reg, X_train_pc, df["TARGET"])
-    y.append(spearmanr(df["TARGET"].values, y_predict).correlation)
-plt.plot(range(1, len(input_cols)), y, '-o')
-plt.show()
-"""
+def maximize_pca():
+    lin_reg = LinearRegression()
+    y = []
+    for i in range(1, len(input_cols)):
+        pca = PCA(n_components=i)
+        X_train_pc = pca.fit_transform(df[input_cols])
+        y_predict = cross_val_predict(lin_reg, X_train_pc, df["TARGET"], cv=cv)
+        y.append(spearmanr(df["TARGET"].values, y_predict).correlation)
+    plt.plot(range(1, len(input_cols)), y, '-o')
+    plt.show()
+
 
 # transform the datas using PCA
 pca = PCA(n_components=19)
 new_df = pd.DataFrame(pca.fit_transform(df[input_cols]))
 input_cols = new_df.columns
-new_df["TARGET"] = df["TARGET"]
+new_df["TARGET"] = df["TARGET"].values
 df = new_df
 
 # get the best columns to perform a regression analysis
@@ -67,25 +73,28 @@ K_best = SelectKBest(f_regression, k=8)
 K_best.fit_transform(df[input_cols].values, df["TARGET"])
 reg_cols = df[input_cols].columns[K_best.get_support()]
 
+
 # optimize the hyperparameter of the models
-""" 
-y = []
-K_best = SelectKBest(f_regression, k=8)
-K_best.fit_transform(df[input_cols].values, df["TARGET"])
-reg_cols = df[input_cols].columns[K_best.get_support()]
-for i in range(1, len(input_cols) + 1):
-    model = DecisionTreeRegressor(max_depth=i)
-    y_predict = cross_val_predict(model, df[input_cols], df["TARGET"])
-    y_test = df["TARGET"].values
-    y.append(spearmanr(y_test, y_predict).correlation)
-    print(i)
-plt.plot(range(1, len(input_cols) + 1), y, '-o')
-plt.show()
-"""
+def optimize_hyperparameter():
+    cvs = []
+    for i in range(100):
+        cvs.append(KFold(n_splits=10, shuffle=True, random_state=random.randint(1, 1000)))
+    y = []
+    for i in range(1, 100):
+        y.append([])
+        for j in range(100):
+            model = Ridge(i)
+            y_predict = cross_val_predict(model, df[reg_cols], df["TARGET"], cv=cvs[j])
+            y_test = df["TARGET"].values
+            y[-1].append(spearmanr(y_test, y_predict).correlation)
+        y[-1] = np.mean(y[-1])
+        print(i)
+    plt.plot(range(1, 100), y, '-o')
+    plt.show()
 
 
 def test_model(model, columns):
-    y_predict = cross_val_predict(model, df[columns], df["TARGET"])
+    y_predict = cross_val_predict(model, df[columns], df["TARGET"], cv=cv)
     y_test = df["TARGET"].values
     print("R2 score =", round(r2_score(y_test, y_predict), 4))
     print("spearman =", round(spearmanr(y_test, y_predict).correlation, 4))
@@ -96,15 +105,17 @@ def test_model(model, columns):
 print("Linear regression:")
 test_model(LinearRegression(), reg_cols)
 print("Ridge regression:")
-test_model(Ridge(1.7), reg_cols)
+test_model(Ridge(50), reg_cols)
 print("Lasso regression:")
 test_model(Lasso(857), input_cols)
 print("K-neighbors regression:")
 test_model(KNeighborsRegressor(72), reg_cols)
 print("Decision tree regression:")
 test_model(DecisionTreeRegressor(max_depth=12), input_cols)
+"""
 print("Random forest regression:")
-test_model(RandomForestRegressor(n_estimators=100), input_cols)
+test_model(RandomForestRegressor(max_depth=12, n_estimators=100), input_cols)
+"""
 
 # get and clean the validation data
 df_test = pd.read_csv('csv_files/DataNew_X.csv')
@@ -114,14 +125,12 @@ df_test[cols] = StandardScaler().fit_transform(df_test[cols])
 df_test.replace(to_replace=["FR", "DE"], value=[0, 1], inplace=True)
 input_cols = [e for e in df_test.columns if e not in ('ID', 'DAY_ID', 'TARGET')]
 new_df_test = pd.DataFrame(pca.transform(df_test[input_cols]))
-new_df_test["ID"] = df_test["ID"]
+new_df_test["ID"] = df_test["ID"].values
 df_test = new_df_test
-input_cols = [e for e in df_test.columns if e != 'ID']
-
 
 # create prediction for the validation data
 Y_test_submission = df_test[['ID']].copy()
-test_model = LinearRegression()
+test_model = Ridge(50)
 test_model.fit(df[reg_cols], df["TARGET"])
 Y_test_submission['TARGET'] = test_model.predict(df_test[reg_cols])
 
